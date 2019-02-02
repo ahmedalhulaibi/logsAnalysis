0df13f96d6eaa5e8405e2e68ae911a9c04cda83b
ahmedalhulaibi <ahmed.alhulaibi41@gmail.com>
2019-02-02 12:48:13 -0500

    refactor: Formatting to pep8 code style

--- report.py
+++ report.py
@@ -13,15 +13,17 @@ ERR_TEMPLATE = '''%s -- %3.2f%% errors'''
 def main(argv):
     print("-----%s Most Popular Articles-----\n" % argv[0])
     articles = "\r\n".join(VIEW_TEMPLATE % (article, views)
-                           for article, views in get_popular_articles(argv[0]))
+                                    for article, views in get_popular_articles(argv[0]))
     print(articles)
     print("\n-----%s Most Popular Authors-----\n" % argv[1])
     authors = "\r\n".join(VIEW_TEMPLATE % (author, views)
                           for author, views in get_popular_authors(argv[1]))
     print(authors)
     pct = float(argv[2]) / 100.0
-    print("\n-----Days HTTP Error Status Exceeded %3.2f%%-----\n" % (pct*100))
-    days = "\r\n".join(ERR_TEMPLATE % (day.strftime("%B %d, %Y"), percentage)
+    print(
+        "\n-----Days HTTP Error Status Exceeded %3.2f%%-----\n" %
+        (pct * 100))
+    days = "\r\n".join(ERR_TEMPLATE % (day.strftime("%B %d, %Y"), percentage) 
                        for day, percentage in get_errs_day_pct(pct))
     print(days)
     return
